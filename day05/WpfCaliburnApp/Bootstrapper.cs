2d28d952142b9a7bec9bf4faf818a11de1c487c8
ROJE100 <couch148@naver.com>
2022-06-07 17:40:46 +0900

    220607 커밋

---
+++ day05/WpfCaliburnApp/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using WpfCaliburnApp.ViewModels;
+
+namespace WpfCaliburnApp
+{
+    /// <summary>
+    /// 시작 윈도우 지정 클래스!!!
+    /// </summary>
+    public class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();    //Bootstrapper를 초기화 해주는 메서드
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ day05/WpfCaliburnApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Caliburn.Micro;
+
+namespace WpfCaliburnApp.ViewModels
+{
+    public class MainViewModel:Screen //반드시 MainViewModel 클래스가 필요
+    {
+
+    }
+}
