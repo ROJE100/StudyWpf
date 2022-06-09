310130d66e2883b06b2d14fd81bb807edd507da2
ROJE100 <couch148@naver.com>
2022-06-09 16:55:14 +0900

    220609 SmartHome 포트폴리오 시작

---
+++ portfolio/WpfPortfolio/WpfSmartHomeMonitoringApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfSmartHomeMonitoringApp.ViewModels
+{
+    public class MainViewModel:Conductor<object> //Screen에는 ActivateItem[Async] 메서드가 없음
+    {
+        public MainViewModel()
+        {
+            DisplayName = "SmartHome Monitoring v2.0"; // 윈도우 타이틀, 제목
+        }
+
+        public void LoadDataBaseView()
+        {
+            ActivateItemAsync(new DataBaseViewModel());
+        }
+
+        public void LoadHistoryView()
+        {
+            ActivateItemAsync(new HistoryViewModel());
+        }
+        public void ExitProgram()
+        {
+            Environment.Exit(0); //오류 없이 프로그램 종료 0 
+        }
+    }
+}
